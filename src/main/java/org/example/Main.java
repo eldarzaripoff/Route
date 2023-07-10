cf32b523284a2dfbb14b4475f54093d40f844125
eldar <eldar.zaripoff@gmail.com>
2023-07-11 00:49:20 +0300

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,69 +6,87 @@ import java.util.Random;
 
 public class Main {
     public static Map<Integer, Integer> sizeToFreq = new HashMap<>();
-    public static final Object lock = new Object();
+    public static final int LENGTH = 100;
+    public static final int AMOUNT_OF_THREADS = 1000;
+    public static final String LETTERS = "RLRFR";
 
     public static void main(String[] args) {
-        Thread[] threads = new Thread[100];
-        for (int i = 0; i < 100; i++) {
-            threads[i] = new Thread(() -> System.out.println(generateRoute("RLRFR", 100)));
-            threads[i].start();
+        Thread[] threads = new Thread[AMOUNT_OF_THREADS];
+        Thread printer = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    printLeader();
+                }
+            }
+        });
+        printer.start();
+
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
+            threads[i] = getThread();
 
         }
-        for (Thread thread : threads
-        ) {
+        for (Thread thread : threads) {
             try {
+                thread.start();
                 thread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        new Thread(() -> {
-            synchronized (lock) {
-                for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-                    int key = entry.getKey();
-                    int value = entry.getValue();
-                    System.out.println("- " + key + " повторений " + value + " раз");
-                }
-            }
-
-        }).start();
+        printer.interrupt();
     }
 
     public static String generateRoute(String letters, int length) {
-        int count = 0;
-        int maxCount = 0;
-        int frequency = 0;
-        int times = 1;
+
         Random random = new Random();
         StringBuilder route = new StringBuilder();
         for (int i = 0; i < length; i++) {
             route.append(letters.charAt(random.nextInt(letters.length())));
         }
-        for (int i = 0; i < route.toString().toCharArray().length; i++) {
-            if (route.toString().toCharArray()[i] == 'R') {
-                count++;
-                maxCount++;
-            } else {
-                if (count > 0) {
-                    frequency++;
-                }
-                count = 0;
-            }
-        }
-        if (route.toString().toCharArray()[route.toString().toCharArray().length - 1] == 'R') frequency++;
-        System.out.println("'R' symbols quantity is " + maxCount + " " + frequency + " times");
-        synchronized (lock) {
-            for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-                int key = entry.getKey();
-                if (maxCount != key) {
-                    sizeToFreq.put(key, 1);
-                } else {
-                    times++;
-                    sizeToFreq.put(key, times);
-                }
-            }
-        }
+
         return route.toString();
     }
+
+    public static Thread getThread() {
+        return new Thread(() -> {
+            int count = 0;
+            int frequency = 0;
+            String route = generateRoute(LETTERS, LENGTH);
+            for (int i = 0; i < route.toCharArray().length; i++) {
+                if (route.toCharArray()[i] == 'R') {
+                    count++;
+                } else {
+                    if (count > 0) {
+                        frequency++;
+                    }
+                    count = 0;
+                }
+            }
+            if (route.toCharArray()[route.toCharArray().length - 1] == 'R') frequency++;
+            synchronized (sizeToFreq) {
+                if (sizeToFreq.containsKey(frequency)) {
+                    sizeToFreq.put(frequency, sizeToFreq.get(frequency) + 1);
+                } else {
+                    sizeToFreq.put(frequency, 1);
+                }
+                sizeToFreq.notify();
+            }
+        });
+    }
+
+    public static void printLeader() {
+        Map.Entry<Integer, Integer> max = sizeToFreq
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get();
+        System.out.println("Текущий лидер " + max.getKey()
+                + " (встретилось " + max.getValue() + " раз)");
+    }
+
 }
